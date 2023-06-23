4d15831a15ab72815a95fd95181c85740a535cc7
matrix-bro <razmhz11@gmail.com>
2023-06-23 12:58:52 +0545

    Created Models - Person and Passport

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Passport, Person
+
+
+admin.site.register(Person)
+admin.site.register(Passport)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Passport(models.Model):
+    passport_no = models.CharField(max_length=20, unique=True)
+    person = models.OneToOneField(Person, on_delete=models.CASCADE, related_name="passport")
+
+    def __str__(self):
+        return self.passport_no
