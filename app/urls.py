f666e9078ae26afc10e793dcd758112bb0fa0b59
matrix-bro <razmhz11@gmail.com>
2023-06-24 14:19:50 +0545

    Created AllPersonView and made a route

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from app.views import AllPersonView
+
 urlpatterns = [
-    # path(),
+    path('person/', AllPersonView.as_view(), name='person-list'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from app.models import Person
+
+class AllPersonView(APIView):
+    class OutputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Person
+            fields = ['id', 'name']
+
+    def get(self, request):
+        person_list = Person.objects.all()
+        serializer = self.OutputSerializer(person_list, many=True)
+        serialized_data = serializer.data
+
+        return Response(
+            {
+                "success": True,
+                "message": "All Person retrieved successfully.",
+                "data": serialized_data,
+                "code": status.HTTP_200_OK,
+            }
+        )
+
+
+        
+
+            
