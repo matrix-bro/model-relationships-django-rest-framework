cff8a6e2850dd9cc18af04fff606cb6099febf49
matrix-bro <razmhz11@gmail.com>
2023-06-24 14:34:15 +0545

    Created AllPassportView and a route for that

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.views import AllPersonView
+from app.views import AllPassportView, AllPersonView
 
 urlpatterns = [
     path('person/', AllPersonView.as_view(), name='person-list'),
+    path('passports/', AllPassportView.as_view(), name='passport-list'),
 ]
--- app/views.py
+++ app/views.py
@@ -32,6 +32,26 @@ class AllPersonView(APIView):
             }
         )
 
+class AllPassportView(APIView):
+    class OutputSerializer(serializers.ModelSerializer):
+        
+        class Meta:
+            model = Passport
+            fields = ['id', 'passport_no', ]
+
+    def get(self, request):
+        passports = Passport.objects.all()
+        serializer = self.OutputSerializer(passports, many=True)
+        serialized_data = serializer.data
+
+        return Response(
+            {
+                "success": True,
+                "message": "All Passports retrieved successfully.",
+                "data": serialized_data,
+                "code": status.HTTP_200_OK,
+            }
+        )
 
         
 
