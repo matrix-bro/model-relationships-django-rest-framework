fff94286fbf6ee0329947ea87c48487f75242aaa
matrix-bro <razmhz11@gmail.com>
2023-06-25 11:45:28 +0545

    Displayed Authors along with all Books

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from rest_framework import serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from app.models import Author, Passport, Person
+from app.models import Author, Book, Passport, Person
 
 class AllPersonView(APIView):
     class OutputSerializer(serializers.ModelSerializer):
@@ -64,9 +64,16 @@ class AllPassportView(APIView):
 class AllAuthorView(APIView):
     class OutputSerializer(serializers.ModelSerializer):
                 
+        class BookSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Book
+                fields = ['id', 'title',]
+
+        books = BookSerializer(many=True)
+
         class Meta:
             model = Author
-            fields = ['id', 'name', ]
+            fields = ['id', 'name', 'books', ]
 
     def get(self, request):
         authors = Author.objects.all()
