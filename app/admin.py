adf07b701de9184de945a5b02b4b0b4fa5d6a248
matrix-bro <razmhz11@gmail.com>
2023-06-25 11:39:58 +0545

    Created AllAuthorView and its route

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from app.models import Passport, Person
+from app.models import Author, Book, Passport, Person
 
 
 admin.site.register(Person)
 admin.site.register(Passport)
+admin.site.register(Author)
+admin.site.register(Book)
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from rest_framework import serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from app.models import Passport, Person
+from app.models import Author, Passport, Person
 
 class AllPersonView(APIView):
     class OutputSerializer(serializers.ModelSerializer):
@@ -60,6 +60,25 @@ class AllPassportView(APIView):
             }
         )
 
-        
 
+class AllAuthorView(APIView):
+    class OutputSerializer(serializers.ModelSerializer):
+                
+        class Meta:
+            model = Author
+            fields = ['id', 'name', ]
+
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = self.OutputSerializer(authors, many=True)
+        serialized_data = serializer.data
+
+        return Response(
+            {
+                "success": True,
+                "message": "All Author retrieved successfully.",
+                "data": serialized_data,
+                "code": status.HTTP_200_OK,
+            }
+        )
             
