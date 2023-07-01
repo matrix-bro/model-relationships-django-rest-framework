fed250efb62ca77207b9d517e73f954a12fb4e6a
matrix-bro <razmhz11@gmail.com>
2023-07-01 12:28:10 +0545

    AllPassportView - Passport Details Update

--- app/api/passport.py
+++ app/api/passport.py
@@ -72,3 +72,71 @@ class AllPassportView(APIView):
             "data": serializer.data,
             "code": status.HTTP_201_CREATED,
         })
+    
+    """
+        Update: Updates Passport details of a Person
+    """
+    def put(self, request):
+        passport_id = request.query_params.get("id")
+        person_id = request.query_params.get("person_id")
+
+        person = Person.objects.filter(id=person_id).first()
+        passport = Passport.objects.filter(id=passport_id).first()
+
+        if not person:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Person not found.",
+                    "code": status.HTTP_400_BAD_REQUEST,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        if not passport:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Passport not found.",
+                    "code": status.HTTP_400_BAD_REQUEST,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        # First check if the old person is same as the person (to be updated)
+        old_person = passport.person
+        if old_person != person:
+
+            # Check if person already has a passport
+            try: 
+                if person.passport:
+                    return Response(
+                        {
+                            "success": False,
+                            "message": "Person already has a Passport.",
+                            "code": status.HTTP_400_BAD_REQUEST,
+                        },
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+            except Passport.DoesNotExist:
+                return Response(
+                        {
+                            "success": False,
+                            "message": f"Person with Id={person_id} doesnot have a Passport yet. First, create a Passport.",
+                            "code": status.HTTP_400_BAD_REQUEST,
+                        },
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+
+        serializer = self.InputSerializer(passport, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save(person=person)
+
+        return Response({
+            "success": True,
+            "message": "Passport details updated successfully.",
+            "data": serializer.data,
+            "code": status.HTTP_201_CREATED,
+        })
